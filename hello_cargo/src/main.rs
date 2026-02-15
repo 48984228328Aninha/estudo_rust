7abc931e4b955ed5149274098d3808fa067cadc8
ar8405710 <ar8405710@gmail.com>
2026-02-15 20:26:39 +0000

    adicionei a opção de checked para evitar int overflow

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    //usando wrapping p evitar int overflow
     let x:u8 = 255;
 
     let result = x.wrapping_add(1);
@@ -8,4 +9,16 @@ fn main() {
         println!["{}", resultado];
 
         println!("{x}");
+
+    //agr vou usar checked, o + seguro
+
+    let y:u8 = 255;
+    let checar = y.checked_add(1);
+
+    match checar {
+        Some(valor) => println!("Resultado: {}", valor),
+        None => println!("Erro"),
+    }
+
+
 }
