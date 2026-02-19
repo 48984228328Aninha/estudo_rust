7f878c45ec6a41ed02b8afa51e05af83ef9a8405
ar8405710 <ar8405710@gmail.com>
2026-02-19 15:13:15 +0000

    add loop

---
+++ fluxo_control/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let number: u8 = 254;
+
+    let checar = number.checked_add(1);
+
+    match checar {
+        Some(number) => println!("Resultado: {}", number),
+        None => println!("Erro"),
+    }
+
+    if number == 254 {
+        println!("é igual");
+    } else {
+        println!("não é igual");
+    }
+
+    //usando if em uma instrução let
+    let condition = true;
+    let check = if condition {254} else {0};
+
+    println!("The value of number is: {number}, {check}");
+
+    //loop executa várias vezes até eu mandar parar
+    loop {
+        println!("loop");
+    }
+}
