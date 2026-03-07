3d72b4526e2cd130423a96e8d2eb3de9345c6560
ar8405710 <ar8405710@gmail.com>
2026-03-07 00:03:46 +0000

    adicionei takes_and_gives_back e gives_ownership

--- propriedade/src/main.rs
+++ propriedade/src/main.rs
@@ -3,6 +3,10 @@ fn main() {
 
     let n: i32 = 5;
 
+    let s1 = gives_ownership();
+    let s2 = String::from("olá");
+    let s3 = takes_and_gives_back(s2);
+
 
 takes_ownership(s);
 makes_copy(n);
@@ -14,4 +18,13 @@ fn takes_ownership(some_string: String) {
 
 fn makes_copy(some_integer: i32) {
     println!("Some integer: {some_integer}");
+}
+
+fn gives_ownership() -> String {
+    let alguma_string = String::from("Olá");
+    alguma_string
+}
+
+fn takes_and_gives_back(string: String) -> String {
+    string
 }
