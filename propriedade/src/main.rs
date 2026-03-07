573ba33cddabd69b7831d30d932d6876d8948a50
ar8405710 <ar8405710@gmail.com>
2026-03-07 16:09:31 +0000

    adicionar cálculo de tamanho de tupla

--- propriedade/src/main.rs
+++ propriedade/src/main.rs
@@ -1,16 +1,27 @@
 fn main() {
     let s = String::from("Hello");
 
+    let s1 = String::from("Ola");
+    let (s2, len) = calculate_length(s1);
+
     let n: i32 = 5;
 
     let s1 = gives_ownership();
     let s2 = String::from("olá");
     let s3 = takes_and_gives_back(s2);
 
+    println!("s1: {s1}, s3: {s3}");
+
 
 takes_ownership(s);
 makes_copy(n);
 }
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
+} 
 
 fn takes_ownership(some_string: String) {
     println!("Some String: {some_string}");
