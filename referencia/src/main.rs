b672c08761f22d31844c2a021ecb92376d1ea33c
AMANDA RODRIGUES DE SIQUEIRA <amanda.siqueira3@alunos.sc.senac.br>
2026-03-23 16:33:18 -0300

    fix: adicionar referência vazia

--- referencia/src/main.rs
+++ referencia/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
 
     let mut s = String::from("hello");
 
+    let referencia_para_o_nada = soltar();
+
     modifica(&mut s);
 
     println!("The size of {s1} is: {len}");
@@ -24,6 +26,29 @@ fn modifica(uma_string: &mut String) {
     uma_string.push_str("longa");
 }
 
-/*Dois ou mais ponteiros acessam o mesmo dado ao mesmo tempo.
+fn soltar() -> String {
+    let s = String::from("txt");
+
+    s; //tem que ser sem a referência, se não ela deixa de existir depois do escopo
+    //da função
+
+    //só é possível mudar o valor da referência de uma variável se ela for mutável, 
+    //o mesmo vale para referência
+}
+
+/* condições de data race:
+Dois ou mais ponteiros acessam o mesmo dado ao mesmo tempo.
 Ao menos um dos ponteiros é usado para escrever sobre o dado.
 Não há nenhum mecanismo sendo usado para sincronizar o acesso ao dado. */
+
+//pode:
+
+let r1 = &s;
+let r2 = &s;
+
+//não pode >:(
+/*
+    let r1 = &mut s;
+ */
+
+
