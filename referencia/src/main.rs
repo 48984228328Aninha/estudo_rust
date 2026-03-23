892fb947bc6fc5693e584c658b5c20230d9355ae
AMANDA RODRIGUES DE SIQUEIRA <amanda.siqueira3@alunos.sc.senac.br>
2026-03-23 16:15:19 -0300

    fix: adicionar uma referência mutável

--- referencia/src/main.rs
+++ referencia/src/main.rs
@@ -5,6 +5,10 @@ fn main() {
     let s2 = String::from("iai :)")
     let len2 = calculate_length(&s2);
 
+    let mut s = String::from("hello");
+
+    modifica(&mut s);
+
     println!("The size of {s1} is: {len}");
 
     println!("the size of {s1} is: {len}");
@@ -14,3 +18,12 @@ fn calculate_length(s: &String) -> usize {
     s.len()
 }
 
+// referência mutável
+
+fn modifica(uma_string: &mut String) {
+    uma_string.push_str("longa");
+}
+
+/*Dois ou mais ponteiros acessam o mesmo dado ao mesmo tempo.
+Ao menos um dos ponteiros é usado para escrever sobre o dado.
+Não há nenhum mecanismo sendo usado para sincronizar o acesso ao dado. */
