2068383ec1a6e8844a0673a86ba7cd16f7c2294a
AMANDA RODRIGUES DE SIQUEIRA <amanda.siqueira3@alunos.sc.senac.br>
2026-03-23 15:59:49 -0300

    fix: adicionar mais uma String e função de calcular

--- referencia/src/main.rs
+++ referencia/src/main.rs
@@ -2,9 +2,15 @@ fn main() {
     let s1 = String::from("Ola");
     let len = calculate_length(&s1);
 
+    let s2 = String::from("iai :)")
+    let len2 = calculate_length(&s2);
+
+    println!("The size of {s1} is: {len}");
+
     println!("the size of {s1} is: {len}");
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
 }
+
